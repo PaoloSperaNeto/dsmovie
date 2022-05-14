834dc9496e7badd51c7972055859549a8c68546e
Paolo Spera Neto <paolosperaneto@gmail.com>
2022-05-14 09:00:15 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,18 +1,27 @@
 import axios from "axios";
+import { useState, useEffect } from "react";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 import { BASE_URL } from "utils/requests";
+import { MoviePage } from "types/movie";
 
 function Listing() {
+    
+    const [pageNumber, setPageNumber] = useState(0);
 
-    //forma estática
-    axios.get(`${BASE_URL}/movies?size=12&page=0`).
-    then(Response => {
-        console.log(Response.data);
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
+        .then(response => {
+            const data = response.data as MoviePage;
+            console.log(data);
+            setPageNumber(data.number);
+        });
     });
+    
 
     return (
         <>
+            <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
